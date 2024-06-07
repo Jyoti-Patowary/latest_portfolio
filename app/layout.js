259c8bc12a8f8f3d29055145d90db18d23cff96c
Jyoti-Patowary <jpatowary8@gmail.com>
2024-06-07 16:48:32 +0530

    added google analytics

--- app/layout.js
+++ app/layout.js
@@ -1,16 +1,28 @@
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import Head from "next/head";
+import Script from "next/script";
 
 // const poppins = Poppins({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "DEEP",
+  title: "Jyoti Patowary",
   description: "Portfolio",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html>
+    <Script async src="https://www.googletagmanager.com/gtag/js?id=G-2MXBVN9329"></Script>
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-2MXBVN9329');
+        `}
+      </Script>
       <body style={{fontFamily: 'poppins'}}>{children}</body>
     </html>
   );
